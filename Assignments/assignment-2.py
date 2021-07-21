37fbf827e4aad839d7bb414ad0624dd71e074d1d
Jeet Purohit <62439012+jeet0777@users.noreply.github.com>
2021-07-21 20:02:52 +0530

    detecting hand pos with mediapipe library

---
+++ Assignments/assignment-2.py
@@ -0,0 +1,30 @@
+# hand receognition with mediapipe assignment2
+import cv2
+import mediapipe as mp
+
+mp_drawing=mp.solutions.drawing_utils
+mp_hands=mp.solutions.hands
+cap=cv2.VideoCapture(0)
+with mp_hands.Hands(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+    while cap.isOpened():
+        flag,frame=cap.read()
+        if not flag:
+            print("could'nt acceess camera")
+            break
+        else:
+            frame = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
+            frame.flags.writeable = False
+            results = hands.process(frame)
+            frame.flags.writeable = True
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(
+            frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            cv2.imshow('MediaPipe Hands', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+cap.release()
+cv2.destroyAllWindows()
